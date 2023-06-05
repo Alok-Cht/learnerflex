a45022f1c2e45c7d16122326cc26eeb56e98fd81
Alok Chaudhary <alokchaudharytharu@gmail.com>
2023-06-05 09:03:21 +0545

    base & learnerflex created & configure

--- base/views.py
+++ base/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
+'''rooms = [ 
     {'id':1, 'name':'lets learn python'},
     {'id':2, 'name':'lets learn A.I '},
     {'id':3, 'name':'lets learn Machine Learning'},
     {'id':4, 'name':'lets learn Django'},
 ]
+'''
 
 def home(request):
+    rooms = Room.objects.all() 
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    return render(request, 'room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
+    return render(request, 'base/room.html', context)
